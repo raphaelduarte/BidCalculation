635a5188e951fd6b181740425278afcb0cf27e7a
raphaelreshut <raphael@reshuttech.com>
2024-09-10 23:16:19 -0300

    feat: implement strategy pattern with factory for fee calculation
    - Added FeeCalculatorFactory to select the correct fee calculation strategy based on vehicle type
    - Refactored FeeCalculationUseCase to delegate strategy selection to the factory
    - Registered FeeCalculatorFactory, CommonFeeCalculator, and LuxuryFeeCalculator in DI container

--- BidCalculation.API/Program.cs
+++ BidCalculation.API/Program.cs
@@ -1,6 +1,17 @@
+using BidCalculation.Application.Factories;
+using BidCalculation.Application.UseCases;
+using BidCalculation.Domain.Strategies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<CommonVehicleFeeCalculator>();
+builder.Services.AddTransient<LuxuryVehicleFeeCalculator>();
+
+builder.Services.AddTransient<FeeCalculatorFactory>();
+
+builder.Services.AddTransient<FeeCalculationUseCase>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ BidCalculation.Domain/Strategies/LuxuryVehicleFeeCalculator.cs
@@ -0,0 +1,9 @@
+﻿namespace BidCalculation.Domain.Strategies;
+
+public class LuxuryVehicleFeeCalculator : IFeeCalculator
+{
+    public decimal CalculateFee(decimal basePrice)
+    {
+        return basePrice * 0.05m;
+    }
+}
