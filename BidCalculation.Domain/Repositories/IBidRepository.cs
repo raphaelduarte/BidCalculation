8b2b3307ceaec6adae861fd483b7f6e0c315af6b
raphaelreshut <raphael@reshuttech.com>
2024-09-10 22:11:35 -0300

    feat: implement domain repositories

---
+++ BidCalculation.Domain/Repositories/IBidRepository.cs
@@ -0,0 +1,11 @@
+﻿using BidCalculation.Domain.Entities;
+
+namespace BidCalculation.Domain.Repositories;
+
+public interface IBidRepository
+{
+    Task AddAsync(Bid bid);                           
+    Task<Bid> GetByIdAsync(int id);                   
+    Task<IEnumerable<Bid>> GetAllAsync();
+    Task UpdateAsync(Bid bid);
+}
---
+++ BidCalculation.Domain/Repositories/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿using BidCalculation.Domain.Entities;
+
+namespace BidCalculation.Domain.Repositories;
+
+public interface IVehicleRepository
+{
+    Task AddAsync(Vehicle vehicle);                   
+    Task<Vehicle> GetByIdAsync(int id);               
+    Task<IEnumerable<Vehicle>> GetAllAsync();         
+    Task UpdateAsync(Vehicle vehicle);                
+}
